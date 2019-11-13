d7e7b1e55836d1177c2855f6aa72cb82426c23f8
Root <sylk@protonmail.com>
2019-11-12 19:15:01 -0700

    git init with a few adjustments

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <p>
+      Welcome to BoojBooks, this is the frontend application portion of the test.
+      <br>
+      Documentation for the requirements is available <a href="https://github.com/ActiveWebsite/boojbooks">here</a>
+    </p>
+    <BooksManagement/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BooksManagement from '@/components/BooksManagement.vue';
+
+export default {
+  name: 'home',
+  components: {
+    BooksManagement,
+  },
+};
+</script>
