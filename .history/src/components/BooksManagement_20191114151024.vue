2f50be791c5a0b773769d24a4df48e055bb63892
Root <sylk@protonmail.com>
2019-11-14 16:30:32 -0700

    Comitting history for books management

---
+++ .history/src/components/BooksManagement_20191114151024.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <h3>The Books Available Currently:</h3>
+    <div v-for="book in books" :key="book.id">
+      <book
+        :title="book.title"
+        :author="book.author"
+        :edition="book.edition"
+        :length="book.length"
+        :score="book.score"
+        :cover="book.cover"
+        :file="book.file"
+        :releaseDate="book.releaseDate"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Book from '@/components/Book.vue';
+
+export default {
+  name: 'BooksManagement',
+  components: {
+    Book,
+  },
+  beforeMount() {
+    this.getBooks();
+  },
+  data() {
+    return {
+      books: [],
+    };
+  },
+  methods: {
+    getBooks() {
+      try {
+        const response = axios.get('//plushykingdom.com/api/books');
+        this.books = response.data;
+        console.log(this.books);
+      } catch (error) {
+        // TODO: feed this to a debugging application such as sentry, telescope, etc... 
+        console.log({ error });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ .history/src/components/BooksManagement_20191114162456.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h3>The Books Available Currently:</h3>
+    <div v-for="book in books" :key="book.id">
+      <book
+        :title="book.title"
+        :author="book.author"
+        :edition="book.edition"
+        :length="book.length"
+        :score="book.score"
+        :cover="book.cover"
+        :file="book.file"
+        :releaseDate="book.releaseDate"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Book from '@/components/Book.vue';
+
+export default {
+  name: 'BooksManagement',
+  components: {
+    Book,
+  },
+  beforeMount() {
+    this.getBooks();
+  },
+  data() {
+    return {
+      books: [],
+    };
+  },
+  methods: {
+    async getBooks() {
+      try {
+        const response = await axios.get('https://www.plushykingdom.com/api/books');
+        this.books = response;
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
