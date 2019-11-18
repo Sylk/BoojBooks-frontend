85365952683765b00323f2b5a2b904ece2482ee9
Root <sylk@protonmail.com>
2019-11-17 23:49:59 -0700

    Formatting applied

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,12 @@
   <div class="about">
     <h1>This BoojBooks Demo was created by Sylk/Matthew Bauer</h1>
     <ul>
-      <li>LinkedIn: <a href="https://www.linkedin.com/in/matthewtbauer">https://www.linkedin.com/in/matthewtbauer</a></li>
+      <li>
+        LinkedIn:
+        <a href="https://www.linkedin.com/in/matthewtbauer"
+          >https://www.linkedin.com/in/matthewtbauer</a
+        >
+      </li>
       <li>Github: <a href="https://www.github.com/sylk">https://www.github.com/sylk</a></li>
       <li>Email: <a href="mailto: sylk@protonmail.com ">sylk@protonmail.com </a></li>
       <li>Phone: <a href="tel:1-314-210-0865">(314) 210-0865</a></li>
